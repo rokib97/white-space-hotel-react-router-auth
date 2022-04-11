a6f1cd6412b51b7914dbbe8b751643a1b79c7579
rokib97 <hasanrokib1997@gmail.com>
2022-04-11 21:30:56 +0600

    added navbar and setup router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Pages/About/About";
 import Checkout from "./Pages/Checkout/Checkout";
 import Home from "./Pages/Home/Home/Home";
 import Login from "./Pages/Login/Login";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
@@ -15,6 +19,7 @@ function App() {
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/about" element={<About></About>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,49 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h3>Header</h3>
-    </div>
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand
+            className="fw-bolder"
+            style={{ color: "#222831" }}
+            href="#home"
+          >
+            WHITE SPACE
+          </Navbar.Brand>
+          <Nav className="ms-auto">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active-link" : "link")}
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active-link" : "link")}
+              to="/checkout"
+            >
+              Checkout
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active-link" : "link")}
+              to="/login"
+            >
+              Login
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active-link" : "link")}
+              to="/about"
+            >
+              About
+            </NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
